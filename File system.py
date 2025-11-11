4d67eb758bcad037ef5487b5c79de1c500d0e381
Jake-Elchon <quantummathematics777@gmail.com>
2025-11-11 22:15:25 +0100

    preparing folder class to hold encapsulation functions supporting file system manipulation

--- File system.py
+++ File system.py
@@ -10,12 +10,52 @@ else:
 
 folder_directory = os.getcwd() + "/" + folder_head_name 
 
-# entering the folder and creating folders in it
-folder_count = 10
+'''
+    Creating a file handling system where users enter a command at a certain root folder
+    and delete files, delete folders, or go back to certain folders and enter folders
+'''
 
-for i in range(folder_count):
-    if os.path.exists(folder_directory + "/" + f"folder_{i+1}"):
-        pass
-    else:
-        os.makedirs(folder_directory + "/" + f"folder_{i+1}")
+# defining the file class tree structure 
+class Folder:
+    def __init__(self, name, prev_folder_directory):
+        self.folder_name = name
+        self.previous_folder_directory = directory
+        self.holding_folders: list[Folder] = []
+        self.holding_files: list[str] = []
+        self.total_list: list = []
+        self.folder_index: int = 0
+    
+    def current_folder(self, command: str) -> str:
+        '''
+        return the next folder or file name
+        '''
+        if command == "up":
+            if self.folder_index + 1 > len(self.total_list) - 1:
+                return self.total_list[self.folder_index]
+            else:
+                self.folder_index += 1
+                return self.total_list[self.folder_index + 1]
+        
+        else:
+            if self.folder_index - 1 < 0:
+                return self.total_list[self.folder_index]
+            else:
+                self.folder_index -= 1
+                return self.total_list[self.folder_index + 1]
+            
+    def Add_folder(self, folder: Folder):
+        self.holding_folders.append(folder) # adding folder to list of folders 
+        # renewing the list of folders and files in the current folder
+        self.total_list = []
+        self.total_list.extend(self.holding_folders)
+        self.total_list.extend(self.holding_files)
+        
+    def Add_file(self, filepath: str):
+        self.holding_filess.append(folder) # adding files to list of files 
+        # renewing the list of folders and files in the current folder
+        self.total_list = []
+        self.total_list.extend(self.holding_folders)
+        self.total_list.extend(self.holding_files)
+        
+    
     
