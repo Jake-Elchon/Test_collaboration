5d7d6e52e298437d2bb716bd8f6bc872ea099b45
Jake-Elchon <quantummathematics777@gmail.com>
2025-11-11 21:43:51 +0100

    implementation of two layer file system

--- File system.py
+++ File system.py
@@ -3,8 +3,19 @@ import os
 folder_head_name = "root_folder"
 
 # make the directory
-os.makedirs(folder_head_name)
+if os.path.exists(folder_head_name):
+    pass
+else:
+    os.makedirs(folder_head_name)
 
-# listing all the folders inside our current directory
-for folders in os.listdir(os.curdir):
-    print(folders)
+folder_directory = os.getcwd() + "/" + folder_head_name 
+
+# entering the folder and creating folders in it
+folder_count = 10
+
+for i in range(folder_count):
+    if os.path.exists(folder_directory + "/" + f"folder_{i+1}"):
+        pass
+    else:
+        os.makedirs(folder_directory + "/" + f"folder_{i+1}")
+    
